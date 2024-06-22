c6558c6259de6fe1f05bc53e596f0fdf77a8c6e2
Carlos Vega <carlosevega03@gmail.com>
2024-06-21 23:04:49 -0400

    Write the logic to get the computer choice
    Adds the getComputerChoice() function to the
    JavaScript file. This function utilizes Math.random()
    to generate a choice for the computer opponent in
    "Rock, Paper, Scissors".

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+// Randomly generates a number to fetch an RPS move 
+function getComputerChoice() {
+    let randNum = Math.random();
+    let computerChoice;
+
+    if (randNum <= 0.33) {
+        computerChoice = "rock";
+    }
+    else if (randNum <= 0.66) {
+        computerChoice = "paper";
+    }
+    else {
+        computerChoice = "scissors";
+    }
+    
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
