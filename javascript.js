40bb4ffb4257ad4ad52be335c26c2d1d68b997b0
Carlos Vega <carlosevega03@gmail.com>
2024-06-30 23:13:58 -0400

    Give UI Interface to Rock, Paper, Scissor game
    This commit gives the rock, paper, scissor game
    an interactive ui. The player can click buttons to
    input their choice, and a score counter updates to
    keep track of the player's/computer's points.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
 let humanScore = 0;
 let computerScore = 0;
+let rounds = 0;
 let NUM_ROUNDS = 5;
+const selectButton = document.querySelector("#selectButtons");
+const roundOutcome = document.querySelector(".roundOutcome");
+const gameOutcome = document.querySelector(".gameOutcome");
 
 
 // Randomly generates a number to fetch an RPS move 
@@ -38,38 +42,64 @@ function getHumanChoice() {
 // Simulates one round of Rock, Paper Scissors
 function playRound(humanChoice, computerChoice) {
     if (humanChoice == undefined) {
+        roundOutcome.innerHTML = "Invalid Choice";
         console.log("Invalid Choice");
     } 
     else if (humanChoice == computerChoice) {
+        roundOutcome.innerHTML = "It's a Tie!";
         console.log("It's a Tie!");
     }
     else if ((humanChoice == "rock" && computerChoice == "paper") || 
             (humanChoice == "paper" && computerChoice == "scissors") || 
             (humanChoice == "scissors" && computerChoice == "rock")) {
-                console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+                roundOutcome.innerHTML = `You lose! ${computerChoice} beats ${humanChoice}.`;
+                console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
                 computerScore++;
             }
     else {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`)
+        roundOutcome.innerHTML = `You win! ${humanChoice} beats ${computerChoice}.`;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
         humanScore++;
     }
 }
 
-function playGame() {
+function refreshScore() {
+    const player = document.querySelector(".playerScore .pS");
+    player.innerHTML = humanScore;
+    const computer = document.querySelector(".computerScore .cS");
+    computer.innerHTML = computerScore;
+}
+
+function resetGame() {
     humanScore = 0;
     computerScore = 0;
+    rounds = 0;
+    roundOutcome.innerHTML = "";
+    gameOutcome.innerHTML = "";
+}
 
-    for (let i = 0; i < NUM_ROUNDS; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
+function checkFinalScore() {
     if (humanScore > computerScore) {
+        gameOutcome.innerHTML = "You Win!!";
         console.log("You Win!!");
     } else if (humanScore < computerScore) {
+        gameOutcome.innerHTML = "You Lose!!";
         console.log("You Lose!!");
     } else {
-        console.log("It's a Tie!");
+        gameOutcome.innerHTML = "It's a Tie!!";
+        console.log("It's a Tie!!");
     }
 }
 
-playGame();
+
+selectButton.addEventListener("click", (event) => {
+    if (event.target.id != "selectButtons") {
+        if (rounds % NUM_ROUNDS == 0) resetGame();
+        playRound(event.target.id, getComputerChoice());
+        refreshScore();
+        rounds++;
+    
+        if (rounds % NUM_ROUNDS == 0) checkFinalScore();
+    }
+});
+
